84ca542cd6b786b05b46e883b082274fef686cfd
Igor Kapkov <igasgeek@me.com>
2019-10-13 17:13:34 +1100

    cling 0.6

--- Formula/cling.rb
+++ Formula/cling.rb
@@ -2,9 +2,8 @@ class Cling < Formula
   desc "The cling C++ interpreter"
   homepage "https://root.cern.ch/cling"
   url "https://github.com/root-project/cling.git",
-      :tag      => "v0.5",
-      :revision => "0f1d6d24d4417fc02b73589c8b1d813e92de1c3f"
-  revision 2
+      :tag      => "v0.6",
+      :revision => "82ac7bf1870abbedb7fe44f8e34a429538f26a8d"
 
   bottle do
     sha256 "361280b6e0673f196308e51b418955e2eab0df5289c278f5c196936909963363" => :mojave
@@ -16,14 +15,14 @@ class Cling < Formula
 
   resource "clang" do
     url "http://root.cern.ch/git/clang.git",
-        :tag      => "cling-patches-r302975",
-        :revision => "1f8b137c7eb06ed8e321649ef7e3f3e7a96f361c"
+        :tag      => "cling-patches",
+        :revision => "02c41d5edd15232b0b25ec1d842403552c2aceb4"
   end
 
   resource "llvm" do
     url "http://root.cern.ch/git/llvm.git",
-        :tag      => "cling-patches-r302975",
-        :revision => "2a34248cb945d63ded5ee55128e68efd7e5b87c8"
+        :tag      => "cling-patches",
+        :revision => "e0b472e46eb5861570497c2b9efabf96f2d4a485"
   end
 
   def install
